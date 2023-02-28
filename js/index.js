ce96e9e5ff010ea7496cf005752bbf78f1fdcdfe
Arman <hurianarmann@gmail.com>
2023-03-01 01:33:56 +0330

    Add Day 9

--- js/index.js
+++ js/index.js
@@ -17,3 +17,48 @@ const swiper = new Swiper('.swiper', {
     }
 })
 
+const $ = document;
+
+let landingTitle = $.querySelector('.landing__container-title')
+let coursesCount = $.querySelector('#courses-count')
+let minutesCount = $.querySelector('#minutes-count')
+let usersCount = $.querySelector('#user-count')
+
+window.addEventListener('load',()=>{
+    
+    
+    let landingText ='ما به هر قیمتی دوره آموزشی تولید نمیکنیم '
+    
+    let countIndex = 0;
+   typeWriter(countIndex,landingText)
+   numberCunter(40,coursesCount)
+   numberCunter(31_320,minutesCount)
+   numberCunter(31_071,usersCount)
+})
+
+function typeWriter(index,text){
+    // console.log(text.length);
+    // console.log(index);
+    if(index < text.length){
+      landingTitle.innerHTML += text[index]
+      index++
+    }
+    
+    setTimeout(()=>{
+        typeWriter(index,text)
+    }      
+    ,100)
+}
+
+let count = 0;
+
+function numberCunter(max,elem){
+    let interval = setInterval(()=>{
+       count++
+       elem.innerHTML = count
+       if(count === max){
+        clearInterval(interval)
+       }
+    },10)
+}
+
